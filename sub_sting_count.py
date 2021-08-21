b7695fe0f2007106fcf6512206e09f08c069c9bd
Ahmed <ahmedropewala@gofynd.com>
2021-08-21 15:33:27 +0530

    Interview Questions and Kosaraju implemnentation

---
+++ sub_sting_count.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from typing import DefaultDict
+
+
+S = "adcvregtbgchbcrrewath"
+
+W = "cat"
+
+count_s = defaultdict(int)
+count_w = defaultdict(int)
+for i in S:
+    count_s[i] += 1
+
+for j in W:
+    count_w[j] += 1
+
+alpha_count = None
+
+for k in count_w:
+    current_count = count_s[k] // count_w[k]
+    try:
+        if alpha_count > (current_count):
+            alpha_count = current_count
+    except:
+        alpha_count = current_count
+print(alpha_count)
