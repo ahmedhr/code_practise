37db05668f80608fda11ea9ac0e38f199af6bbdf
Ahmed <ahmedropewala@gofynd.com>
2022-01-16 02:49:27 +0530

    Move zeroes

--- move_zeros.py
+++ move_zeros.py
@@ -21,4 +21,47 @@ def moveZeroes(nums) -> None:
     return nums
 
 
+# print(moveZeroes([0, 0, 1, 0, 2, 0, 0, 0, 0, 0]))
+
+# Single pointer
+
+def moveZeroes(nums) -> list:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    zero = None
+    i = 0
+    while i < len(nums):
+        if nums[i] != 0 and zero is not None:
+            nums[zero], nums[i] = nums[i], nums[zero]
+            i = zero
+            zero = None
+        elif nums[i] == 0 and zero is None:
+            zero = i
+        i += 1
+
+    return nums
+
+
+print(moveZeroes([0, 0, 1, 0, 2, 0, 0, 0, 0, 0]))
+
+# Two pointers 
+# O(n)
+def moveZeroes(nums) -> list:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    i, j = 0, 0
+    while j < len(nums):
+        if nums[i] == 0 and nums[j] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+        if nums[i] != 0:
+            i += 1
+        if nums[j] == 0:
+            j += 1
+
+    return nums
+
+
 print(moveZeroes([0, 0, 1, 0, 2, 0, 0, 0, 0, 0]))
